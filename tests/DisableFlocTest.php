53eb048e4227553c1914d31eef0013e8e6d7eef5
freek <freek@spatie.be>
2021-05-05 19:21:54 +0200

    wip

--- tests/DisableFlocTest.php
+++ tests/DisableFlocTest.php
@@ -10,7 +10,9 @@ class DisableFlocTest extends TestCase
     /** @test */
     public function it_will_add_the_floc_header()
     {
-        Route::get('test', fn () => 'Computer says no, Google')->middleware(DisableFloc::class);
+        Route::get('test', function () {
+            return 'Computer says no, Google';
+        })->middleware(DisableFloc::class);
 
         $this
             ->get('test')
