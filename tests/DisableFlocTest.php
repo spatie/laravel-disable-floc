9b214bb5790aec9547a8f5cf30e9abf507d69ae9
freek <freek@spatie.be>
2021-05-05 19:13:37 +0200

    wip

--- tests/DisableFlocTest.php
+++ tests/DisableFlocTest.php
@@ -3,13 +3,14 @@
 namespace Spatie\DisableFloc\Tests;
 
 use Illuminate\Support\Facades\Route;
+use Spatie\DisableFloc\DisableFloc;
 
 class DisableFlocTest extends TestCase
 {
     /** @test */
     public function it_will_add_the_floc_header()
     {
-        Route::get('test', fn () => 'Computer says no, Google');
+        Route::get('test', fn () => 'Computer says no, Google')->middleware(DisableFloc::class);
 
         $this
             ->get('test')
